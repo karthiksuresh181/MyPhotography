6f3a3d3cd471c8ba5c77ae796a7f8ab4d5d7f296
karthiksuresh181 <karthu.karthu1@gmail.com>
2021-01-28 01:15:09 +0530

    added lazy loading to profile comp

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ProfileComponent } from './profile/profile.component';
 import { MainComponent } from './main/main.component';
 
-import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
-  declarations: [WelcomeComponent, ProfileComponent, MainComponent],
+  declarations: [
+    WelcomeComponent,
+    MainComponent
+  ],
   imports: [
-    CommonModule,
-    SharedModule
+    CommonModule
   ],
   exports: [
     MainComponent
--- src/app/home/main/main.component.ts
+++ src/app/home/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('dynamicProfileComponent', { read: ViewContainerRef}) dynamicProfileComponent: ViewContainerRef;
+  constructor(
+    private vcr: ViewContainerRef,
+    private cfr: ComponentFactoryResolver
+  ) { }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
+    let yOffset = window.pageYOffset;
+    if(yOffset > 50 && yOffset<100){
+      if(!this.dynamicProfileComponent.length){
+        this.load_profile_component();
+      }
+    }
+  }
+  
+  async load_profile_component(){
+    const componentFactory = this.cfr.resolveComponentFactory(ProfileComponent);
+    this.dynamicProfileComponent.clear();
+    const dynamicComponent = <ProfileComponent>this.dynamicProfileComponent.createComponent(componentFactory).instance;
+  }
 }
